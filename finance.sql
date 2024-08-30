04aa7d03ff7551145466e07117296027ccb60784
YuHao-Tian <114182493+YuHao-Tian@users.noreply.github.com>
2024-08-30 07:29:49 +0000

    Update finance.sql

--- finance.sql
+++ finance.sql
@@ -1,4 +1,7 @@
 -- Drop the existing stock_transaction table if it exists
+CREATE DATABASE finance;
+USE DATABASE finance;
+
 DROP TABLE IF EXISTS `stock_transaction`;
 
 -- Create the new stock_transaction table with the updated schema
@@ -9,5 +12,11 @@ CREATE TABLE `stock_transaction` (
   `quantity` INT NOT NULL,  -- Number of stocks bought or sold
   `total_amount` DECIMAL(15, 2) NOT NULL,  -- Total transaction amount (quantity * price_per_unit)
   `transaction_type` ENUM('BUY', 'SELL') NOT NULL,
+  `transaction_time`
   FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
 );
+
+Drop TABLE EXISTS `user`;
+
+
+Drop TABLE EXISTS `shareholding`;
